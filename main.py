84af91ab7e82bc60bf511930f543c3bbfe35a3d4
aswinpradeepc <aswinpradeepc@gmail.com>
2023-10-28 20:29:32 +0530

    GPT did something more

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import os
+
 app = FastAPI()
 
+# Use the environment variable directly with a default value
 port = int(os.environ.get("PORT", 10000))
 
 @app.get("/")
-async def root():
+async def read_root():
     return {"message": "Hello World"}
 
 @app.get("/hello/{name}")
@@ -15,4 +17,5 @@ async def say_hello(name: str):
 if __name__ == "__main__":
     import uvicorn
 
+    # Use uvicorn.run directly without if __name__ condition for better compatibility
     uvicorn.run(app, host="0.0.0.0", port=port)
