8b85b569a82e022336d1591f24f3cff8729e2f3b
aswinpradeepc <aswinpradeepc@gmail.com>
2023-10-28 20:21:32 +0530

    did something

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+port = int(os.environ.get("PORT", 10000))
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -9,3 +11,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
